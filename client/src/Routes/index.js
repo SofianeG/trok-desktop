449968834c6436285fb94e77bdc49f46103f454a
SofianeG <sofianegougam17@gmail.com>
2021-02-27 14:46:23 +0100

    add HomePageScreen

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -5,33 +5,33 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
-// import NavBar from '../components/NavBar/NavBar';
+import NavBar from '../components/NavBar/NavBar';
 import HomePage from '../Screens/HomePageScreen/HomePage';
-// import UserPage from '../components/Profil/UserPage';
-// import PersonnalisationScreen from '../Screens/PersonnalisationScreen/PersonnalisationScreen';
-// import SignInForm from '../Screens/SignInFormScreen/SignInForm';
-// import SignUpForm from '../Screens/SignUpFormScreen/SignUpForm';
-// import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
-// import AboutScreen from '../Screens/AboutScreen/AboutScreen';
-// import ParametersScreen from '../Screens/ParametresScreen/ParametersScreen';
+import UserPage from '../components/Profil/UserPage';
+import PersonnalisationScreen from '../Screens/PersonnalisationScreen/PersonnalisationScreen';
+import SignInForm from '../Screens/SignInFormScreen/SignInForm';
+import SignUpForm from '../Screens/SignUpFormScreen/SignUpForm';
+import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
+import AboutScreen from '../Screens/AboutScreen/AboutScreen';
+import ParametersScreen from '../Screens/ParametresScreen/ParametersScreen';
 
 const index = () => {
   return (
     <Router>
       <NavBar />
       <Switch>
-        {/* <Route path="/login" exact component={SignInForm} />
-        <Route path="/register" exact component={SignUpForm} /> */}
+        <Route path="/login" exact component={SignInForm} />
+        <Route path="/register" exact component={SignUpForm} />
         <Route path="/" exact component={HomePage} />
-        {/* <Route path="/user" exact component={UserPage} />
+        <Route path="/user" exact component={UserPage} />
         <Route path="/profile" exact component={ProfileScreen} />
         <Route path="/about" exact component={AboutScreen} />
-        <Route path="/parametres" exact component={ParametersScreen} />
+        <Route path="/parameters" exact component={ParametersScreen} />
         <Route
           path="/personnalisation"
           exact
           component={PersonnalisationScreen}
-        /> */}
+        />
         <Redirect to="/" />
       </Switch>
     </Router>
