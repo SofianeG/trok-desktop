a9bad5d4f69fd5cd3edc91d86d445e15943e2dd0
SofianeG <sofianegougam17@gmail.com>
2021-02-27 13:57:41 +0100

    add assets

---
+++ client/src/assets/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+  btn_google_border: "#D4D4D4",
+  btn_google_text: "#6D7278",
+  btn_facebook: "#4B74FF",
+  btn_continuer: "#0091FF",
+  ajouter_photos: "#0091FF",
+  white: "#ffffff",
+  black: "#000000",
+  green: "#40CE6A",
+  signup_text: "#2E2C38",
+  grey_mot_de_passe_oublié: "#9E9E9E",
+  input_border: "#D4D4D4",
+};
