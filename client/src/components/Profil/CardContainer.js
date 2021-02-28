ae6d272568e11f87ee50baa1d6ecc3a3b119d70f
SofianeG <sofianegougam17@gmail.com>
2021-02-28 12:21:49 +0100

    add Profil_Icon

--- client/src/components/Profil/CardContainer.js
+++ client/src/components/Profil/CardContainer.js
@@ -1,12 +1,12 @@
-import Pen from '../../assets/FicheProfil/icons8-edit-1.svg';
-import Heart from '../../assets/FicheProfil/icons8-heart-2.svg';
-import Hands from '../../assets/FicheProfil/icons8-handshake.svg';
-import Perso from '../../assets/FicheProfil/icons8-tune.svg';
-import Param from '../../assets/FicheProfil/icons8-settings.svg';
-import About from '../../assets/FicheProfil/icons8-about.svg';
-import Conditions from '../../assets/FicheProfil/icons8-bulleted_list.svg';
-import ThumbsUp from '../../assets/FicheProfil/icons8-thumbs_up.svg';
-import Help from '../../assets/FicheProfil/icons8-help.svg';
+import Pen from '../../assets/Profil_Icon/icons8-edit-1.svg';
+import Heart from '../../assets/Profil_Icon/icons8-heart-2.svg';
+import Hands from '../../assets/Profil_Icon/icons8-handshake.svg';
+import Perso from '../../assets/Profil_Icon/icons8-tune.svg';
+import Param from '../../assets/Profil_Icon/icons8-settings.svg';
+import About from '../../assets/Profil_Icon/icons8-about.svg';
+import Conditions from '../../assets/Profil_Icon/icons8-bulleted_list.svg';
+import ThumbsUp from '../../assets/Profil_Icon/icons8-thumbs_up.svg';
+import Help from '../../assets/Profil_Icon/icons8-help.svg';
 
 export const Card1 = [
   {
@@ -60,4 +60,4 @@ export const Card3 = [
   },
 ];
 
-export default {Card1, Card2, Card3};
+export default { Card1, Card2, Card3 };
